a165491dcdaf8d922f9760ceaf5f48ddcd5e7b9f
sitimaesyaroh <136550981+sitimaesyaroh@users.noreply.github.com>
2023-09-22 07:34:05 +0700

    Add files via upload

---
+++ masuk.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="css/style_masuk.css" rel="stylesheet">
+  </head>
+  <body>
+    
+
+    <div class="container">
+      <div class="card form">
+        <div class="card-body">
+            <h2 class="text-center">SISTEM PENDAFTARAN BEASISWA</h1>
+        </div>
+        <div class="card-text text-center">
+           <img src="gambar/logo.jpg" width="100px">
+        </div>
+        <div class="card-text text-center">
+            <a href="admin/login_admin.php">
+                <button class="btn btn-success btn-lg">
+                ADMIN
+                </button>
+            </a>
+            <a href="siswa/login_siswa.php">
+                <button class="btn btn-success btn-lg">
+                MAHASISWA
+                </button>
+            </a>
+        </div>
+      </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>

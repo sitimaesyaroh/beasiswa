04e1f2875447c4198115baba74c9d2017bb2c4ec
sitimaesyaroh <136550981+sitimaesyaroh@users.noreply.github.com>
2023-09-22 07:32:40 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,13 @@
+<div class="container">
+        <div class="row text-center">
+            <div class="col">
+                <h2>Home</h2>
+            </div>
+        </div>
+        <div class="row text-right">
+            <div class="col">
+                <p>Ini Adalah Aplikasi Penerimaan Beasiswa. 
+                    Silahkan Melakukan Pengolahan Data Dengan Menekan Menu Di Atas</p>
+            </div>
+        </div>
+</div>

bbbc9fb004d00ed2e0b1e37a7320d7e3e2b0c8da
derisrinaldi0 <derisrinaldi0@gmail.com>
2023-09-25 08:53:08 +0700

    ipk otomatis dan file extensi hanya pdf,png dan jpg

--- siswa/simpan_pendaftaran.php
+++ siswa/simpan_pendaftaran.php
@@ -19,27 +19,34 @@
             }
 
             $fileName = $file['name'];
+            $allowedFileExtensions = ['pdf','jpg','png']; // extensi yang di izinkan
+            $ext = pathinfo($fileName, PATHINFO_EXTENSION);
 
-            $newFileName = $nis."." . pathinfo($fileName, PATHINFO_EXTENSION); //merubah nama file menjadi $nis
-            $destination = $uploadDir . $newFileName;
+            if(in_array($ext,$allowedFileExtensions)){
+                $newFileName = $nis."." . pathinfo($fileName, PATHINFO_EXTENSION); //merubah nama file menjadi $nis
+                $destination = $uploadDir . $newFileName;
 
-            if (move_uploaded_file($file['tmp_name'], $destination)) {
-                echo "File berhasil diunggah.";
-            } else {
-                echo "Terjadi kesalahan saat mengunggah file.";
-            }
+                if (move_uploaded_file($file['tmp_name'], $destination)) {
+                    echo "File berhasil diunggah.";
+                } else {
+                    echo "Terjadi kesalahan saat mengunggah file.";
+                }
 
-            $perintah = mysqli_query($c,"insert into tb_daftar values('','$tgl','$nis','$email','$no_hp','$semester','$ipk','$pil_beasiswa','$newFileName','B','0')");
-            if($perintah){
-                echo "<script>alert('File berhasil diunggah dan Anda Berhasil Melakukan Pendaftaran.');</script>";
-                echo "<meta http-equiv='refresh' content='0; url=index.php?pg=pendaftaran'>";
-            }
-            else {
-                echo "<script>alert('Maaf Anda Gagal Melakukan Pendaftaran. Silahkan Ulangi Kembali!');</script>";
+                $perintah = mysqli_query($c,"insert into tb_daftar values(null,'$tgl','$nis','$email','$no_hp','$semester','$ipk','$pil_beasiswa','$newFileName','B','0')");
+                if($perintah){
+                    echo "<script>alert('File berhasil diunggah dan Anda Berhasil Melakukan Pendaftaran.');</script>";
+                    echo "<meta http-equiv='refresh' content='0; url=index.php?pg=pendaftaran'>";
+                }
+                else {
+                    echo "<script>alert('Maaf Anda Gagal Melakukan Pendaftaran. Silahkan Ulangi Kembali!');</script>";
+                    echo "<meta http-equiv='refresh' content='0; url=index.php?pg=pendaftaran'>";
+                }
+                
+            }else{
+                echo "<script>alert('File yang diperbolehkan hanya PDF, JPG dan PNG');</script>";
                 echo "<meta http-equiv='refresh' content='0; url=index.php?pg=pendaftaran'>";
             }
         }
 
         
     }
-?>
